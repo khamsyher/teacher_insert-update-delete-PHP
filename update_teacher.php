3367b70548be426796690f846a34396e9265a68a
khamsyher <114057321+khamsyher@users.noreply.github.com>
2023-06-06 17:18:34 +0700

    new update

--- update_teacher.php
+++ update_teacher.php
@@ -1,43 +1,49 @@
 <div class="container">
-  <h2>Edit Teacher</h2>
   <?php
-    include 'config.php';
+    include ('config.php');
     $id = $_GET['t_id'];
     $query = "SELECT * FROM teacher WHERE t_id = '$id'";
     $result = $conn->query($query);
-    $row = $result->fetch_assoc();
+    while($row = $result->fetch_assoc()){
+      ?>
+      <h2>Edit Teacher</h2>
+      <form action="update_teacher.php" method="POST">
+      <input type="hidden" name="t_id" value="<?php echo $row['t_id']; ?>">
+      <div class="form-group">
+        <label for="name">Name:</label>
+        <input type="text" class="form-control" id="name" name="name" value="<?php echo $row['t_name']; ?>">
+      </div>
+      <div class="form-group">
+        <label for="last_name">Last Name:</label>
+        <input type="text" class="form-control" id="last_name" name="last_name" value="<?php echo $row['t_lastname']; ?>">
+      </div>
+      <div class="form-group">
+        <label for="gender">Gender:</label>
+        <select class="form-control" id="gender" name="gender">
+          <option value="Male" <?php if($row['t_gen'] == 'Male') echo 'selected'; ?>>Male</option>
+          <option value="Female" <?php if($row['t_gen'] == 'Female') echo 'selected'; ?>>Female</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="telephone">Telephone:</label>
+        <input type="text" class="form-control" id="telephone" name="telephone" value="<?php echo $row['t_telephone']; ?>">
+      </div>
+      <div class="form-group">
+        <label for="subject">Subject:</label>
+        <input type="text" class="form-control" id="subject" name="subject" value="<?php echo $row['t_subject']; ?>">
+      </div>
+      <button type="submit" name="btn_update" class="btn btn-primary">Update</button>
+    </form>
+      <?php
+    }
+    
   ?>
-  <form action="update_teacher.php" method="POST">
-    <input type="hidden" name="t_id" value="<?php echo $row['t_id']; ?>">
-    <div class="form-group">
-      <label for="name">Name:</label>
-      <input type="text" class="form-control" id="name" name="name" value="<?php echo $row['t_name']; ?>">
-    </div>
-    <div class="form-group">
-      <label for="last_name">Last Name:</label>
-      <input type="text" class="form-control" id="last_name" name="last_name" value="<?php echo $row['t_lastname']; ?>">
-    </div>
-    <div class="form-group">
-      <label for="gender">Gender:</label>
-      <select class="form-control" id="gender" name="gender">
-        <option value="Male" <?php if($row['t_gen'] == 'Male') echo 'selected'; ?>>Male</option>
-        <option value="Female" <?php if($row['t_gen'] == 'Female') echo 'selected'; ?>>Female</option>
-      </select>
-    </div>
-    <div class="form-group">
-      <label for="telephone">Telephone:</label>
-      <input type="text" class="form-control" id="telephone" name="telephone" value="<?php echo $row['t_telephone']; ?>">
-    </div>
-    <div class="form-group">
-      <label for="subject">Subject:</label>
-      <input type="text" class="form-control" id="subject" name="subject" value="<?php echo $row['t_subject']; ?>">
-    </div>
-    <button type="submit" name="btn_update" class="btn btn-primary">Update</button>
-  </form>
+  
 </div>
 <?php 
+include 'config.php';
   if(isset($_POST['btn_update'])){
-    include 'config.php';
+    
     $t_id = $_POST['t_id'];
     $name = $_POST['name'];
     $last_name = $_POST['last_name'];
@@ -55,7 +61,7 @@
                 title: 'Update Data Success!',
                 showConfirmButton: false,
                 timer: 1500
-            })
+            }),
             window.setTimeout(function(){
                 window.location.href = "index.php";
             }, 2000);
